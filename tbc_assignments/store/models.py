941866049b04df8e97bd29efa92fdbd9d048617a
MarBifrost <128312989+MarBifrost@users.noreply.github.com>
2024-10-10 16:11:02 +0400

    Added some changes in models and view files

--- tbc_assignments/store/models.py
+++ tbc_assignments/store/models.py
@@ -1,5 +1,32 @@
+
 from django.db import models
-from django.contrib.auth.models import User
+from pkg_resources import null_ns_handler
+# from django.contrib.auth.models import User
+from versatileimagefield.fields import VersatileImageField
 
 
-# Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    sub_category = models.ManyToManyField('self', blank=True, symmetrical=False)
+
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.IntegerField(null=True)
+    expiry_date = models.DateField(blank=True, null=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
+    photo = VersatileImageField(
+        'image',
+        upload_to='products/',
+        null=True,
+        blank=True,)
+
+    def __str__(self):
+        photo_exist=self.photo if self.photo else None
+        return f'{self.name}, {self.price}, {self.category.name}, {photo_exist}'
--- tbc_assignments/store/views.py
+++ tbc_assignments/store/views.py
@@ -3,6 +3,13 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
 
+from .models import Product, Category
+from django.http import JsonResponse
+from django.core.serializers import serialize
+
+
+
+
 def store(request):
     return render(request, 'store/index.html')
 
@@ -25,3 +32,24 @@ def login_views(request):
 def logout_views(request):
     logout(request)
     return redirect('store:login')
+
+def return_products(request):
+    products = Product.objects.prefetch_related('category').all()
+    res = []
+    for product in products:
+        res.append({
+            'id': product.id,
+            'name': product.name,
+            'price': product.price,
+            'category': product.category.name,
+            'photo': product.photo.url if product.photo else None
+        })
+    return JsonResponse(list(res), safe=False)
+
+
+def return_categories(request):
+    res = Category.objects.all().values('name')
+    return JsonResponse(list(res), safe=False)
+
+
+
