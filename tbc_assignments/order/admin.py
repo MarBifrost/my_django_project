353348dce5f19f9b971d3dceb33e563d500cbb42
MarBifrost <128312989+MarBifrost@users.noreply.github.com>
2024-10-17 17:26:01 +0400

    Created CustomUser, UserCart, defined signals for creating Usercart when Useriscreated and etc.

--- tbc_assignments/order/admin.py
+++ tbc_assignments/order/admin.py
@@ -1,3 +1,12 @@
-from django.contrib import admin
-
 # Register your models here.
+from django.apps import AppConfig
+
+class OrderConfig(AppConfig):
+    default_auto_field = 'django.db.models.AutoField'
+    name = 'order'
+
+    def ready(self):
+        import order.signals
+
+
+
